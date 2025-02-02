604072bb24544a41e9f2c3b590d38cd3fd502724
DevanshGupta-0604 <guptadevansh0604@gmail.com>
2025-02-02 13:15:50 +0530

    feat: Add utils functions for testing

--- test/utils.js
+++ test/utils.js
@@ -1,1 +1,16 @@
+import FAQ from '../models/FAQ/faqSchema';
 
+// Utility function to create a FAQ with optional language
+export const createFAQ = async ({ question = 'What is Node.js?', answer = 'Node.js is a JavaScript runtime.', lang = 'en' } = {}) => {
+  const faq = new FAQ({
+    question,
+    answer,
+    translations: {
+      question_hi: lang === 'hi' ? 'Node.js क्या है?' : undefined,
+      answer_hi: lang === 'hi' ? 'Node.js एक जावास्क्रिप्ट रनटाइम है।' : undefined,
+    },
+  });
+
+  await faq.save();
+  return faq;
+};
