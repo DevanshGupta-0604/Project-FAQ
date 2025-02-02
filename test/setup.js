c5a7ca70582b863606641f5d0a27ece909140dd0
DevanshGupta-0604 <guptadevansh0604@gmail.com>
2025-02-02 11:36:53 +0530

    feat: Setup mongodb memory connection

--- test/setup.js
+++ test/setup.js
@@ -1,1 +1,25 @@
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import server from '../server'; //Express app
 
+let mongoServer;
+
+before(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+
+  // Connect to the in-memory database
+  await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  server.listen(8000); // Start the app after DB connection
+});
+
+after(async () => {
+  await mongoose.disconnect(); // Disconnect from the DB
+  await mongoServer.stop();    // Stop the in-memory server
+});
+
+beforeEach(async () => {
+  const FAQ = require('../models/FAQ/faqSchema');
+  await FAQ.deleteMany({}); // Clear all FAQ entries before each test
+});
